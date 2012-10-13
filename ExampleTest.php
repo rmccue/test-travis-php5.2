05eb02183f6cb5a44bd723a813d59bf781a93a78
Ryan McCue <me@ryanmccue.info>
2012-10-13 13:47:20 +1000

    See what's in this autoloader

--- ExampleTest.php
+++ ExampleTest.php
@@ -1,5 +1,8 @@
 <?php
 
+$contents = file_get_contents('/home/travis/.phpenv/versions/5.2.17/pear/File/Iterator/Autoload.php');
+echo $contents;
+
 class ExampleTest extends PHPUnit_Framework_TestCase {
 	public function testBasicPass() {
 		$this->assertTrue(true);
